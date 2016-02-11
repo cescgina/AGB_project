37156feb83abc7d97101ca35c7d80ff43d1e9f29
Lluís Revilla <lluis.revilla@gmail.com>
2016-02-11 18:00:08 +0100

    Add calculus about the probabilities
    It a bit messy, the part inside the loop of files is  the cleaner one

--- first_script.R
+++ first_script.R
@@ -5,8 +5,8 @@ tumors <- c("brca", "kirck", "prad", "coad", "luad", "thca", "hnsc", "lusc")
 files <- paste0(tumors, final_txt)
 
 # A list to store the dataframes of each cancer
-tumor_training <- c()
-tumor_test <- c()
+tumor_training <- data.frame()
+tumor_test <- data.frame()
 for (file in files){
   
   # Read file and prepare data
@@ -17,18 +17,33 @@ for (file in files){
   tumor <- data[, grep("T$", names(data))]
   
   # Classify all the values
-  tumor[] <- lapply(tumor, cut, c(-Inf, -2, 2, Inf), c("down", "no_change", "up"))
+  tumor[] <- lapply(tumor, factorize)
   
   #Split for training and testing purposes
   mig <- dim(tumor)[2]/2
   tumor_training <- c(tumor_training, tumor[,c(1:mig)])
   tumor_test <- c(tumor_test, tumor[,c((mig+1):(mig*2))])
   
-  # Start analysis
-  summary(tumor_training)
+  # Count how many of each type are and assign them to the name of the cancer
+  # This way we can calculate the probability of up, down or no_change in a tumor
+  assign(file, table(unlist(tumor_training)))
+  assign(file, table(unlist(tumor_test)))
   
+  #Calculates the probability of each gene to have each type of expression
+  occurences <- apply(tumor_training, 1, table)
+  total <- data.frame(do.call(rbind, occurences))
+  
+  # Join the data with previous data
+  tumor_training <- rbind(tumor_training, get(file))
+  tumor_test <- rbind(tumor_test, get(file))
 }
-training <- data.frame()
+
+
+
+occurences2 <- lapply(occurences2, function(x) {length(x) <- 3; x})
+d <- data.frame(do.call(rbind, occurences2))
+d$user_id <- rownames(d)
+
 first <- read.csv("brca_gene_zscore_full-filtered.txt", sep="\t", 
                   header=TRUE, nrows = 20, row.names=1)
 
@@ -50,6 +65,14 @@ second_tumor_test <- second_tumor[,c((mig+1):(mig*2))]
 # second_tumor_training[second_tumor_training$"X6323T" <= -2,] <- "down"
 # second_tumor_training[second_tumor_training$"X6323T" <= 2 & -2 >= second_tumor_training$"X6323T",] <- "no_change"
 
+
+factorize <- function(invec) {
+  # from: http://stackoverflow.com/a/35338274/
+  factorized <- rep("no_change", length(invec))
+  factorized[invec > 2]  <- "up"
+  factorized[invec < -2]  <- "down"
+  factor(factorized, c("down", "no_change", "up"))
+}
 
 classifier <- function(col){
   colum <- c()
