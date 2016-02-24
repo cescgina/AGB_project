f6d731076c56a9bd5bca6a52351ca9d849b3bf5e
Lluís Revilla <lluis.revilla@gmail.com>
2016-02-24 11:44:42 +0100

    Making it human-friendly when running

--- first_script.R
+++ first_script.R
@@ -3,7 +3,7 @@
 
 args <- commandArgs(trailingOnly=TRUE)
 n_args <- length(args)
-if (n_args <= 5){
+if (n_args <= 6){
   msg = "This program trains Naïve Bayes clasificator and work with the data.\n"
   msg2 = "-d Set the working directory.\n"
   msg3 = "-c Set the names of the cancers to analyze: brca kirc prad in lowercase
@@ -21,23 +21,28 @@ if (n_args <= 5){
   wd <- args[2]
   tumors <- args[c(4:(length(args)-2))]
   output <- args[length(args)]
+  pseudocount = FALSE
 }
 
 # TODO: Remove the follwing line:
-stop("Protect our computer to run the whole programm'by accident' ", call.=FALSE)
+# stop("Protect our computer to run the whole programm'by accident' ", call.=FALSE)
 # Quitar valor
 
+# Things to work with
+# wd <- "../project_data"
+# tumors <- c("brca", "kirc", "prad", "coad", "luad", "thca", "hnsc", "lusc")
+
 # Setting work envrionment
-wd <- "../project_data"
 setwd(wd)
 
 # Initial options
 final_txt <- "_gene_zscore_full-filtered.txt"
-tumors <- c("brca", "kirc", "prad", "coad", "luad", "thca", "hnsc", "lusc")
+
 rownames_m <- c('up', 'no_change', 'down')
 
 # Needed to classify the data.frame when we read it
 factorize <- function(invec) {
+  # Classify the values to up, down or no_change dependiing on their values
   # from: http://stackoverflow.com/a/35338274/
   factorized <- rep("no_change", length(invec))
   factorized[invec > 2]  <- "up"
@@ -79,6 +84,7 @@ funct3 <- function(stat, pr, ge, datase){
   }
   return(0)
 }
+
 # Iterates over the states (up, down, no_change)
 funct2 <- function(dataset, state, pro, gen){
   if (gen %in% colnames(dataset)){
@@ -93,14 +99,19 @@ funct1 <- function(gene, dataset_list, states, prob){
   sum(sapply(dataset_list, funct2, state = states, pro = prob, gen = gene))
 }
 
+### Starting the analysis
 
 for (cancer in tumors){
   
-  if (file.exists(paste0(cancer, "_training_RDS.bin"))){
-    next
-  }
+  # Reading the files if prepared files already exist then omit reading them
+  # (it is very slow to prepare the data about 4 minuts and much memory RAM)
+#   if (file.exists(paste0(cancer, "_training_RDS.bin"))){
+#     next
+#   }
+  
   # Read file and prepare data
   file <- paste0(cancer, final_txt)
+  cat("Reading file ", file, "\n")
   dataset <- read.csv(file, sep="\t", header=TRUE, nrows = 20531, row.names = 1)
   dataset <- na.omit(dataset)
   
@@ -108,6 +119,7 @@ for (cancer in tumors){
   dataset <- split_names(dataset)
   
   # Eliminate normal patients by keeping those ending with T
+  cat("Removing healthy patients\n")
   tumor <- dataset[, grep("T$", names(dataset))]
   rm(dataset)
   
@@ -119,12 +131,14 @@ for (cancer in tumors){
   # for all genes
   selected<-sample(ncol(tumor), 210) 
   tumor_training <- tumor[,selected] 
-  tumor_test <- tumor[,-selected] 
+  tumor_test <- tumor[,-selected]
+  cat("Saving the testing data of ", cancer, "\n")
   saveRDS(tumor_test, file=paste0(cancer,"_test_RDS.bin"))
   rm(tumor)
   
   # Calculates the number of times each gene have each type of expression for 
   # each dataset
+  cat("Calculating the frequencies of ", cancer, "\n")
   freq_training <- apply(tumor_training, 1, function(x)
     tabulate(factor(x, levels = rownames_m), length(rownames_m)))
   row.names(freq_training) <- rownames_m
@@ -132,7 +146,8 @@ for (cancer in tumors){
   # Adds one as a pseudocount before doing any probability
   if (pseudocount){
     freq <- apply(freq_training, c(1,2), pseudocounts)
-  } 
+  }
+  cat("Saving prepared training data of ", cancer, "\n")
   saveRDS(freq_training, file=paste0(cancer,"_training_RDS.bin"))
   
   #From time to time it is advaisable to restart the session to free memory of the
@@ -145,11 +160,11 @@ data_g <- matrix(0, nrow = 3, ncol = 20531)
 
 # Read the prepared data
 for (cancer in tumors){
-  assign(cancer, readRDS(paste0(cancer, "_training_RDS.bin")))
+  # assign(cancer, readRDS(paste0(cancer, "_training_RDS.bin")))
 
 # Calculates the probability of being up, down or no_change regardless of which 
 # type of cancer  it has been 
-
+  cat("Calculating the probability of being up, down or no_change for each gene.\n")
   col_cancer <- colnames(get(cancer))
   mcol <- match(col_cancer, colnames(data_g))
   if (length(col_cancer) == 20531){
@@ -164,11 +179,14 @@ P_g <- t(t(data_g) / patient_gene) # Calculates the probability of a gene to be 
 # down or no_change taking into account all the tumor patients
 
 # Total number of patients with tumor in the training set
+ cat("Calculating the number of patient by cancer and the total number of patients\n")
 patient_tumor <-  sapply(tumors, function(x)sum(get(x)[,1]))
 total_n_patients <- sum(patient_tumor)
 
 # Caclulates the probability of a gene to be up, down or no_change given it is 
 # of a given tumor, ie the conditional probability of a gene
+cat("Calculating the conditional probability of being up, down or no_change given a cancer\n",
+    "Calculates the prior probability\n")
 Pg_tumor <- c() # Probability of genes of a given tumor of being up, down or no_
 P_cancer <- c() # Stores the prior probability 
 for (cancer in tumors){
@@ -184,21 +202,24 @@ for (cancer in tumors){
 # Calculates the probability of being up, down or no_change and of a tumor
 # i.e it is not the conditional probability of a given cancer,  
 # the joint probability
+
 Pg_tumor_state <- c()
 
 for (cancer in tumors){
+  cat("Calculates the joint probability o f being up, down or no_change and being of tumor", cancer, ".\n")
   name_v <- paste0("Pg_", cancer, "_state")
   assign(name_v, apply(get(cancer), c(1,2), probability, 
                        n_patient = total_n_patients))
   Pg_tumor_state <- c(Pg_tumor_state, c=name_v)
 }
 
+cat("Calculating the entropy\n")
 entropy <- sum(sapply(P_cancer,  partial_entropy))
 
 # Calculate the partial entropy for all the genes 
 results <- sapply(col_cancer, funct1, dataset_list = mget(Pg_tumor_state), 
                    states = rownames_m, prob = P_g)
-
+cat("Calculating the mutual information of each gene\n")
 MI <- results + entropy
 saveRDS(MI, "mutual_information.bin")
 
@@ -207,7 +228,7 @@ quan <- quantile(MI)
 threshold <- quan[4]+1.5*(quan[4]-quan[2])
 relevant_genes <- names(MI[MI>threshold])
 
-
+# Selecting with which genes are worth using
 for (cancer in tumors) {
   temp_tumor = readRDS(paste0(cancer,"_test_RDS.bin"))
   relevant_genes<- intersect(relevant_genes,rownames(temp_tumor))
@@ -216,63 +237,60 @@ for (cancer in tumors) {
 # information of the expressions of each gene.
 #tumor_test has rownames=patients and colnames=genes
 #Probabilites in Pg_cancername
-predict <- function(patient,tumors,tumor_test,best_genes,cancer_probs){
-  max=-1e6
-  max_cancer=''
+predict <- function(patient, tumors, tumor_test, best_genes, cancer_probs){
+  max <- -1e6
+  max_cancer <- ''
   for (cancer in tumors){
-    Probs = get(paste0("Pg_",cancer))
-    P = log2(cancer_probs[cancer])
+    Probs <- get(paste0("Pg_",cancer)) # TODO: Everything should go as an argument!
+    P <- log2(cancer_probs[cancer])
     for (genes in best_genes){
-      p=tumor_test[genes,patient]
-#       if (genes == "136542"){
-#         ##Bug: some genes have NA values, in prad file gene 136542 has NA, since we omit
-#         #NAs, the program is not able to find a probability to do the prediction
-          ##Also we will need pseudocounts, as the probability of some genes to be in a 
-          #particular state may be 0
-#         print(p)
-#         print(genes)
-#         print(cancer)
-#         print(Probs[p,genes])
-#       }
+      p <- tumor_test[genes,patient]
       if (Probs[p,genes] == 0){
-        logval = 0
+        logval <-  0
       }
       else {
-        logval = log2(Probs[p,genes])
+        logval <-  log2(Probs[p,genes])
       }
-      #logval = tryCatch((log2(Probs[p,genes])),error= function(e){0})
-      P = P + logval
+      P <-  P + logval
     }
     if (P > max){
-      max = P
-      max_cancer = cancer
+      max <-  P
+      max_cancer <-  cancer
     }
   } 
   return(c(max, max_cancer,"target",patient))
 }
 
-output_dataset = data.frame(Score=numeric(),Prediction=character(),Label=character(),Patient=character(),stringsAsFactors = FALSE)
-Pg_dataset = mget(Pg_tumor_state)
+# The dataset to store the predictions and score of the testing data 
+output_dataset <-  data.frame(Score = numeric(),
+                              Prediction = character(),
+                              Label=character(),
+                              Patient=character(),
+                              stringsAsFactors = FALSE)
+
+Pg_dataset <-  mget(Pg_tumor_state)
 for (tumor_type in tumors){
-  temp_tumor = readRDS(paste0(tumor_type,"_test_RDS.bin"))
+  temp_tumor <-  readRDS(paste0(tumor_type,"_test_RDS.bin"))
   for (patient in colnames(temp_tumor)){
-    prediction = predict(patient,tumors,temp_tumor,relevant_genes,P_cancer)
-    prediction[3]=as.character(tumor_type)
+    prediction <-  predict(patient, tumors, temp_tumor, relevant_genes, P_cancer)
+    prediction[3] <- as.character(tumor_type)
     output_dataset[nrow(output_dataset)+1,] <- prediction
   }
-  print(paste0("Finished with ",tumor_type))
+  cat("Finished with ",tumor_type)
 }
 #colnames(output_dataset) <- c("Score","Prediction","Label","Patient")
 
+# To test the sensivity of the data:
 #Calculate True Positives and False Positives
 positives<-function(x,y){
-  if (x==y){
+  if (x == y){
     return(1)
   }
   else  {
     return(0)
   }
 }
+
 positv <- apply(output_dataset, 1, function(x) {positives(x[2], x[3])})
-TP = sum(positv)
-FP = length(positv) - TP
+TP <-  sum(positv)
+FP <-  length(positv) - TP
